5acdbbe0881e7c2288257a3c25baf0f0f98dd6a3
hujinliang <kennyhu@tencent.com>
2023-01-12 14:34:28 +0800

    chore: modify

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,6 @@ const routes = [
   {
     path: '/player',
     name: 'Player',
-    // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Player')
